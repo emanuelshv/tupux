6a28f1991953517dd6c6406e817fe8fe465b5951
emanuel.shv <emanuel.shv@gmail.com>
2010-02-02 00:00:35 +0000

    Actualización de generación de ficheros con la clase asociativa. Actualización de los proyectos para poder trabajar con VS 2008

--- TUPUX.Estimation/Action/Gallery/Action2LF2RET.cs
+++ TUPUX.Estimation/Action/Gallery/Action2LF2RET.cs
@@ -4,6 +4,7 @@ using System.Text;
 using TUPUX.Entity;
 using TUPUX.Estimation.Relationship;
 using TUPUX.Estimation.File;
+using TUPUX.ActiveRecord;
 
 namespace TUPUX.Estimation.Action.Gallery
 {
@@ -20,39 +21,73 @@ namespace TUPUX.Estimation.Action.Gallery
             UMLClass b=null;
             int defaultDetsA = 0;
             int defaultDetsB = 0;
-            PreFile temp;
+            PreFile temp, tempA, tempB;
 
             RelationshipHelper.GetClasses(r, ref a, ref b, this.IsAlternate, ref defaultDetsA, ref defaultDetsB);
 
             //1st File
-            if ((temp = PreFileHelper.GetPreFileWithClass(a, prefiles)) == null)
+            if ((tempA = PreFileHelper.GetPreFileWithClass(a, prefiles)) == null)
             {
-                temp = new PreFile();
-                temp.Rets.Add(new PreRET());
-                temp.Rets[0].Classes.Add(a);                
-                prefiles.Add(temp);
+                tempA = new PreFile();
+                tempA.Rets.Add(new PreRET());
+                tempA.Rets[0].Classes.Add(a);
+                prefiles.Add(tempA);
             }
-            temp.DefaultDets += defaultDetsA;
+            tempA.DefaultDets += defaultDetsA;
 
             //2nd File
-            if ((temp = PreFileHelper.GetPreFileWithClass(b, prefiles)) == null)
+            if ((tempB = PreFileHelper.GetPreFileWithClass(b, prefiles)) == null)
             {
-                temp = new PreFile();
-                temp.Rets.Add(new PreRET());
-                temp.Rets[0].Classes.Add(b);                
-                prefiles.Add(temp);
+                tempB = new PreFile();
+                tempB.Rets.Add(new PreRET());
+                tempB.Rets[0].Classes.Add(b);                
+                prefiles.Add(tempB);
             }
-            temp.DefaultDets += defaultDetsB;
+
+            tempB.DefaultDets += defaultDetsB;
 
             //3rd File (only if there is an Association-Class)
             if (r is UMLAssociation)
             {
                 if (((UMLAssociation)r).AssociationClass != null)
                 {
-                    temp = new PreFile();
-                    temp.Rets.Add(new PreRET());
-                    temp.Rets[0].Classes.Add(((UMLAssociation)r).AssociationClass);
-                    prefiles.Add(temp);
+                    UMLClass dependencyClass = null;
+                    UMLClass associationClass = ((UMLAssociation)r).AssociationClass;
+                    UMLDependencyCollection dependencies = associationClass.GetDependencies();
+                    
+                    foreach (UMLDependency d in dependencies)
+                    {
+                        d.Client = Helper.GetDependencyClient<UMLClass>(d.Guid);
+                        d.Supplier = Helper.GetDependencySupplier<UMLClass>(d.Guid);
+
+                        if ((d.Client.Guid == associationClass.Guid && d.Supplier.Guid == a.Guid) ||
+                            (d.Supplier.Guid == associationClass.Guid && d.Client.Guid == a.Guid))
+                            dependencyClass = a;
+                        else if ((d.Client.Guid == associationClass.Guid && d.Supplier.Guid == b.Guid) ||
+                            (d.Supplier.Guid == associationClass.Guid && d.Client.Guid == b.Guid))
+                            dependencyClass = b;
+                    }
+                    if (dependencyClass == null)
+                    {
+                        //temp = PreFileHelper.GetPreFileWithClass(a, prefiles);
+                        //if (temp == null)
+                        temp = new PreFile();
+                        temp.Rets.Add(new PreRET());
+                        temp.Rets[0].Classes.Add(((UMLAssociation)r).AssociationClass);
+                        prefiles.Add(temp);
+                    }
+                    else if (dependencyClass == a)
+                    {
+                        PreRET pret = new PreRET();
+                        pret.Classes.Add(associationClass);
+                        tempA.Rets.Add(pret);
+                    }
+                    else if (dependencyClass == b)
+                    {
+                        PreRET pret = new PreRET();
+                        pret.Classes.Add(associationClass);
+                        tempB.Rets.Add(pret);
+                    }
                 }
             }
         }
